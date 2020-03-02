c1b5dc39bc2a1ca9dfbd93bb14ddc6f9906a13c0
kevinLee <arpeggiogio@gmail.com>
2020-03-02 12:26:47 +0900

    React Route App tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Home, About, User } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/user">User</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route exact="true" path='/' component={ Home } />
+          <Route path='/about' component={ About } />
+          <Route path='/user' component={ User } />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
